0bc0f2225806be6ae108cf7f1933d9bae4e9f343
Przemysław Paliński <palinskiprzemyslaw97@gmail.com>
2022-07-10 17:40:46 +0200

    Deployment commit

--- bot.py
+++ bot.py
@@ -9,105 +9,83 @@ dotenv_path = join(dir_path, '.env')
 load_dotenv(dotenv_path)
 
 # credentials
-TMI_TOKEN = os.environ.get('TMI_TOKEN')
-CLIENT_ID = os.environ.get('CLIENT_ID')
+TOKEN = os.environ.get('TOKEN')
 BOT_NICK = os.environ.get('BOT_NICK')
 BOT_PREFIX = os.environ.get('BOT_PREFIX')
 CHANNEL = os.environ.get('CHANNEL')
 
-bot = commands.Bot(
-    irc_token=TMI_TOKEN,
-    client_id=CLIENT_ID,
-    nick=BOT_NICK,
-    prefix=BOT_PREFIX,
-    initial_channels=[CHANNEL]
-)
 
-Users = {}
-isGradingActive = False
+class Bot(commands.Bot):
+    Users = {}
+    isGradingActive = False
 
+    def __init__(self):
+        # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
+        # prefix can be a callable, which returns a list of strings or a string...
+        # initial_channels can also be a callable which returns a list of strings...
+        super().__init__(token=TOKEN, prefix=BOT_PREFIX, initial_channels=[CHANNEL])
 
-@bot.event
-async def event_ready():
-    """ Runs once the bot has established a connection with Twitch """
-    print(f"{BOT_NICK} is online!")
+    async def event_ready(self):
+        """ Runs once the bot has established a connection with Twitch """
+        print(f"{BOT_NICK} is online!")
 
+    @commands.command(name='start')
+    async def start_grade(self, ctx):
+        if ctx.author.is_mod:
+            global isGradingActive, Users
+            Users = {}
+            isGradingActive = True
 
-@bot.event
-async def event_message(ctx):
-    if ctx.author.name.lower() == BOT_NICK.lower():
-        return
-    message_cut = ctx.content.split()
-    message_check = ''
-    if len(message_cut) == 1:
-        message_check = ctx.content
-    elif message_cut[0] == '!score':
-        message_check = ctx.content.split()[0]
-
-    if message_check in ['!start', '!stop', '!score', '!last']:
-        await bot.handle_commands(ctx)
-
-
-@bot.command(name='start')
-async def start_grade(ctx):
-    if ctx.author.is_mod:
+    @commands.command(name='stop')
+    async def stop_grade(self, ctx):
         global isGradingActive, Users
-        Users = {}
-        isGradingActive = True
+        if ctx.author.is_mod and len(Users) > 0 and isGradingActive:
+            isGradingActive = False
+            all_grades = 0
+            for val in Users.values():
+                all_grades += val
+            new_maximum_val = max(Users.keys(), key=(lambda new_k: Users[new_k]))
+            new_minimum_val = min(Users.keys(), key=(lambda new_k: Users[new_k]))
+            await ctx.send(f'Score: {round(all_grades/len(Users),6)} Voters: {len(Users)} '
+                           f'High: {Users[new_maximum_val]} ({new_maximum_val}) '
+                           f'Low: {Users[new_minimum_val]}({new_minimum_val})')
+        elif ctx.author.is_mod and len(Users) == 0:
+            isGradingActive = False
+            await ctx.send(f'No votes or no grading started')
 
+    @commands.command(name='last')
+    async def last_grade(self, ctx):
+        global Users
+        if ctx.author.is_mod and len(Users) > 0:
+            all_grades = 0
+            for val in Users.values():
+                all_grades += val
+            new_maximum_val = max(Users.keys(), key=(lambda new_k: Users[new_k]))
+            new_minimum_val = min(Users.keys(), key=(lambda new_k: Users[new_k]))
+            await ctx.send(f'Score: {round(all_grades / len(Users), 6)} Voters: {len(Users)} '
+                           f'High: {Users[new_maximum_val]} ({new_maximum_val}) '
+                           f'Low: {Users[new_minimum_val]} ({new_minimum_val})')
+        if ctx.author.is_mod and len(Users) == 0:
+            await ctx.send(f'No votes on previous grade')
 
-@bot.command(name='stop')
-async def stop_grade(ctx):
-    global isGradingActive, Users
-    if ctx.author.is_mod and len(Users) > 0 and isGradingActive:
-        isGradingActive = False
-        all_grades = 0
-        for val in Users.values():
-            all_grades += val
-        new_maximum_val = max(Users.keys(), key=(lambda new_k: Users[new_k]))
-        new_minimum_val = min(Users.keys(), key=(lambda new_k: Users[new_k]))
-        await ctx.send(f'Score: {round(all_grades/len(Users),6)} Voters: {len(Users)} '
-                       f'High: {Users[new_maximum_val]} ({new_maximum_val}) '
-                       f'Low: {Users[new_minimum_val]}({new_minimum_val})')
-    elif ctx.author.is_mod and len(Users) == 0:
-        isGradingActive = False
-        await ctx.send(f'No votes or no grading started')
-
-
-@bot.command(name='last')
-async def last_grade(ctx):
-    global Users
-    if ctx.author.is_mod and len(Users) > 0:
-        all_grades = 0
-        for val in Users.values():
-            all_grades += val
-        new_maximum_val = max(Users.keys(), key=(lambda new_k: Users[new_k]))
-        new_minimum_val = min(Users.keys(), key=(lambda new_k: Users[new_k]))
-        await ctx.send(f'Score: {round(all_grades / len(Users), 6)} Voters: {len(Users)} '
-                       f'High: {Users[new_maximum_val]} ({new_maximum_val}) '
-                       f'Low: {Users[new_minimum_val]} ({new_minimum_val})')
-    if ctx.author.is_mod and len(Users) == 0:
-        await ctx.send(f'No votes on previous grade')
-
-
-@bot.command(name='score')
-async def on_add(ctx):
-    global isGradingActive, Users
-    user = ctx.author.name
-    print(ctx.message.content)
-    if isGradingActive and ctx.author.name not in Users:
-        command_string = ctx.message.content
-        command_string = command_string.replace('!score', '').strip()
-        try:
-            value = float(command_string)
-            value = round(value, 3)
-        except ValueError:
-            value = -1
-        if 10 >= value >= 0:
-            Users[user] = value
-            print(f'{len(Users)} and user: {user} score added')
+    @commands.command(name='score')
+    async def on_add(self, ctx):
+        global isGradingActive, Users
+        user = ctx.author.name
+        print(ctx.message.content)
+        if isGradingActive and ctx.author.name not in Users:
+            command_string = ctx.message.content
+            command_string = command_string.replace('!score', '').strip()
+            try:
+                value = float(command_string)
+                value = round(value, 3)
+            except ValueError:
+                value = -1
+            if 10 >= value >= 0:
+                Users[user] = value
+                print(f'{len(Users)} and user: {user} score added')
 
 
 if __name__ == "__main__":
-    # launch bot
+    bot = Bot()
     bot.run()
